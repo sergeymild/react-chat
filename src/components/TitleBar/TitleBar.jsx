12927305f4f7057b7c3e2f7fd004d311b89a4081
hliejun <liejun.hlj@gmail.com>
2018-09-23 18:35:15 +0800

    Added title bar

--- src/components/TitleBar/TitleBar.jsx
+++ src/components/TitleBar/TitleBar.jsx
@@ -3,53 +3,74 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import { AppContext } from '../App/Context.jsx';
+import Avatar from '../Avatar/Avatar.jsx';
 import LazyImage from '../LazyImage/LazyImage.jsx';
 
 import style from './TitleBar.scss';
 
-class TitleBar extends React.Component {
+const TitleBar = (props) => {
+  const { avatar, className, roomId, subtitle, title } = props;
+  const { onInfo, onReturn } = props;
+  return (
+    <AppContext.Consumer>
+      {(context) => (
+        <div className={cx(
+          `chat-title-bar--${context.theme}`,
+          className,
+          style['chat-title-bar']
+        )}>
+          {getBackButton(onReturn, roomId)}
+          {getHeading(title, subtitle)}
+          {getInfoButton(avatar, title, onInfo, roomId)}
+        </div>
+      )}
+    </AppContext.Consumer>
+  );
+};
 
-  /* Lifecycle */
+const getBackButton = (onReturn, roomId) => onReturn ? (
+  <button
+    className={cx(style['chat-title-bar__back-button'])}
+    onClick={onReturn.bind(null, roomId)}
+  >
+    <LazyImage
+      label='back'
+      loader='icon'
+      placeholder='back'
+    />
+  </button>
+) : null;
 
-  constructor (props) {
-    super(props);
-    this.state = {
+const getHeading = (title, subtitle) => (
+  <div className={cx(style['chat-title-bar__heading'])}>
+    <span className={cx(style['chat-title-bar__title'])}>
+      {title}
+    </span>
+    {subtitle && (
+      <span className={cx(style['chat-title-bar__subtitle'])}>
+        {subtitle}
+      </span>
+    )}
+  </div>
+);
 
-    };
-  }
-
-  componentDidMount = () => {
-
-  };
-
-  componentWillUnmount = () => {
-
-  };
-
-  componentDidUpdate = () => {
-
-  };
-
-  render = () => {
-    const { className } = this.props;
-    return (
-      <AppContext.Consumer>
-        {(context) => (
-
-        )}
-      </AppContext.Consumer>
-    );
-  };
-
-}
+const getInfoButton = (avatar, title, onInfo, roomId) => onInfo ? (
+  <Avatar
+    className={cx(style['chat-title-bar__avatar-button'])}
+    name={title}
+    onClick={onInfo.bind(null, roomId)}
+    source={avatar}
+  />
+) : null;
 
 TitleBar.propTypes = {
   avatar: PropTypes.string,
   className: PropTypes.string,
   onInfo: PropTypes.func,
   onReturn: PropTypes.func,
+  roomId: PropTypes.string.isRequired,
   subtitle: PropTypes.string,
-  title: Proptypes.string
+  title: PropTypes.string
 };
 
 TitleBar.defaultProps = {
